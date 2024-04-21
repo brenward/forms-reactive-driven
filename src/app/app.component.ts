5c43543cd324b7e24f3c4dd00e42e0e540eb3227
brenward <bren.ward@gmail.com>
2024-04-21 16:36:04 +0100

    #Setting up form in template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
